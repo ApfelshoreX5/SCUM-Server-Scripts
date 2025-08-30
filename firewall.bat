a85383f690b6ec3884e846a30490e828aebb7b86
ApfelshoreX5 <marco.-b@t-online.de>
2025-08-30 16:12:24 +0200

    Add files via upload

---
+++ firewall.bat
@@ -0,0 +1,26 @@
+@echo off
+:: Check for administrative privileges
+net session >nul 2>&1
+if %errorlevel% neq 0 (
+    echo Requesting administrative privileges...
+    powershell -Command "Start-Process '%~f0' -Verb runAs"
+    exit /b
+)
+
+echo Opening SCUM server ports in Windows Firewall...
+
+:: Query Port - 7778 UDP
+netsh advfirewall firewall add rule name="SCUM Query Port" dir=in action=allow protocol=UDP localport=7778
+
+:: Server Port - 7777 UDP
+netsh advfirewall firewall add rule name="SCUM Server Port" dir=in action=allow protocol=UDP localport=7777
+
+:: Client Port - 7779 TCP
+netsh advfirewall firewall add rule name="SCUM Client Port" dir=in action=allow protocol=TCP localport=7779
+
+:: ICMP
+netsh advfirewall firewall add rule name="Allow ICMPv4 Echo Request" dir=in action=allow protocol=ICMPv4 localport=any 
+
+echo.
+echo Ports opened successfully.
+pause
---
+++ start-server.bat
@@ -0,0 +1,23 @@
+@echo off
+set ServerPath=C:\servers\scum\SCUM\Binaries\Win64
+set SteamCMDPath=C:\servers\SteamCMD
+set InstallPath=C:\servers\scum
+
+:: Server Stop
+:: taskkill /IM "SCUMServer.exe" /F /T
+
+:: Check and update SCUM server
+"%SteamCMDPath%\steamcmd.exe" +force_install_dir "%InstallPath%" +login anonymous +app_update 3792580 validate +quit
+
+cd /d "%ServerPath%"
+start SCUMServer.exe -log -nobattleye
+
+:: Additional startup arguments:
+::
+:: -port=7000           : Start server on specified port (default is 7779)
+::                        Connection port will be port+2
+::
+:: -MaxPlayers=64       : Override max players set in ServerSettings.ini
+::
+:: -nobattleye          : Launch server without Battleye (not recommended)
+pause
